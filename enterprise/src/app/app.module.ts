bc93ebf3f9e1cd0e47b7067f4d5b44df42d39a8c
priscila duarte <pgcosta@stefanini.com>
2022-06-08 09:15:41 -0300

    atualizações

--- enterprise/src/app/app.module.ts
+++ enterprise/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { EdicaoComponent } from './edicao/edicao.component';
+import { VisualizacaoComponent } from './visualizacao/visualizacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    ListagemComponent,
+    EdicaoComponent,
+    VisualizacaoComponent
   ],
   imports: [
     BrowserModule,
